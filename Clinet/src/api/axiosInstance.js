49a0b0fcb9fb2d04037235b2600d30f75cde1864
Manjeet Kumar Maurya <manjeetmaurya7785@gmail.com>
2026-06-23 15:05:36 +0530

    Update axiosInstance.js

--- Clinet/src/api/axiosInstance.js
+++ Clinet/src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://indiatradeoverseas.onrender.com/api';
+const API_BASE_URL = 'https://indiatradeoverseas-1.onrender.com/api';
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
